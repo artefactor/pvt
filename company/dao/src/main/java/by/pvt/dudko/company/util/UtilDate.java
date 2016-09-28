e9bdff74260977382abf6b6f4f6857ebe030c3b4
artefactor <tgx@yandex.ru>
2016-09-29 00:11:44 +0300

    Update UtilDate.java

--- company/dao/src/main/java/by/pvt/dudko/company/util/UtilDate.java
+++ company/dao/src/main/java/by/pvt/dudko/company/util/UtilDate.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 @Component
+// XXX better DateUtil. Better CompanyDateUtil
 public class UtilDate {
 	
 	private static final Logger log = Logger.getLogger(UtilDate.class);
@@ -14,11 +15,13 @@ public class UtilDate {
 	public static Date date(String date) {
 		log.info("(attempt) change type string on the date(my method)");
 		Date Date = new Date();
+		// XXX shoud be extracted to constant at least. Better to load it from property file
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 		try {
 			Date = sdf.parse(date.trim());
 			log.info(" change type string on the date(my method) succesfully");
 		} catch (ParseException e) {
+			// XXX not the approach. Need to throw your own bisiness exception.
 			log.error("Error change type string on the date(my method) "+e);
 		}
 		return Date;
